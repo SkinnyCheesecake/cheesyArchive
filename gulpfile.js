76a99ba53df9f972d5041d520e9cd3cad98fabca
SkinnyCheesecake <rnwdavel4blr@gmail.com>
2024-07-25 19:02:47 -1000

    It's almost done, Performance has been upgraded succesfully, only thing left is some content and a video.

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,9 @@ import { dest, watch, src, series } from 'gulp'
 import gulpSass from 'gulp-sass'
 import * as dartSass from 'sass'
 import { glob } from 'glob'
-
+import fs from 'fs'
+import path from 'path'
+import sharp from 'sharp'
 
 const sass = gulpSass(dartSass)
 
@@ -17,7 +19,7 @@ export function css ( done ) {
 
 //Funcion para crear la carpeta que renderiza el app.js
 export function js ( done ) {
-    src( 'source/js/app.js' ).pipe( dest( 'build/js' ) )
+    src( 'source/js/**/*.js' ).pipe( dest( 'build/js' ) )
     done()
 }
 
@@ -28,4 +30,61 @@ export function renderFunction () {
 
 }
 
-export default series(js, css, renderFunction)
+export async function crop ( done ) {
+    const inputFolder = 'source/imgs/gallery/full'
+    const outputFolder = 'source/imgs/gallery/thumb';
+    const width = 250;
+    const height = 180;
+    if (!fs.existsSync(outputFolder)) {
+        fs.mkdirSync(outputFolder, { recursive: true })
+    }
+    const images = fs.readdirSync(inputFolder).filter(file => {
+        return /\.(jpg)$/i.test(path.extname(file));
+    });
+    try {
+        images.forEach(file => {
+            const inputFile = path.join(inputFolder, file)
+            const outputFile = path.join(outputFolder, file)
+            sharp(inputFile) 
+                .resize(width, height, {
+                    position: 'centre'
+                })
+                .toFile(outputFile)
+        });
+
+        done()
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export async function pictures( done ){
+    const srcDir = './source/imgs';
+    const buildDir = './build/img';
+    const images =  await glob('./source/imgs/**/*{jpg,png}')
+
+    images.forEach(file => {
+        const relativePath = path.relative(srcDir, path.dirname(file));
+        const outputSubDir = path.join(buildDir, relativePath);
+        process(file, outputSubDir);
+    });
+    done();
+}
+
+function process(file, outputSubDir) {
+    if (!fs.existsSync(outputSubDir)) {
+        fs.mkdirSync(outputSubDir, { recursive: true })
+    }
+    const baseName = path.basename(file, path.extname(file))
+    const extName = path.extname(file)
+    const outputFile = path.join(outputSubDir, `${baseName}${extName}`)
+    const outputFileWebp = path.join(outputSubDir, `${baseName}.webp`)
+    const outputFileAvif = path.join(outputSubDir, `${baseName}.avif`)
+
+    const options = { quality: 80 }
+    sharp(file).jpeg(options).toFile(outputFile)
+    sharp(file).webp(options).toFile(outputFileWebp)
+    sharp(file).avif().toFile(outputFileAvif)
+}
+
+export default series(crop, js, css, pictures, renderFunction)
--- source/js/app.js
+++ source/js/app.js
@@ -1,15 +1,146 @@
 document.addEventListener('DOMContentLoaded', function() {
     gallery();
+    navegacion();
+    enlace();
+    unclick();
+    sectionSl();
 })
+
+
+function sectionSl () {
+    const navLinks = document.querySelectorAll('.links a');
+
+    navLinks.forEach(link => {
+        link.addEventListener('click', e => {
+            e.preventDefault();
+
+
+            const sectionScroll = e.target.getAttribute('href');
+            const section = document.querySelector(sectionScroll);
+
+            section.scrollIntoView({behavior: 'smooth'});
+        });
+    });
+}
+
+function enlace () {
+    document.addEventListener('scroll', function () {
+        const section = document.querySelectorAll('section')
+        const links = document.querySelectorAll('.links a')
+
+        let actual = ''
+
+        section.forEach(section =>{
+            const top = section.offsetTop;
+            const height = section.clientHeight;
+
+            if(window.scrollY >= (top - height / 3)){
+                actual = section.id
+            }
+        })
+
+        links.forEach(link =>{
+            link.classList.remove('actual');
+            if(link.getAttribute('href') === '#' + actual){
+                link.classList.add('actual');
+            }
+        })
+    })
+}
+
+function navegacion() {
+    const header = document.querySelector('.header');
+    const firstSection = document.querySelector('.firstSection');
+
+    window.addEventListener('scroll', function() {
+        if(firstSection.getBoundingClientRect().bottom < 1){
+            header.classList.add('fixed'); //Esto es para que la barra de navegación se quede en la parte superior de la pantalla
+        }
+        else{
+            header.classList.remove('fixed');
+        }
+    })
+}
 
 function gallery () {
     const galeria = document.querySelector('.galeria-imagenes')
     const CANTIDAD_DE_IMAGENES = 16;
     for(let i = 1; i <= CANTIDAD_DE_IMAGENES; i++){
-        const imagen = document.createElement('IMG');
-        imagen.src = `source/imgs/${i}.jpg`;
+        const imagen = document.createElement('PICTURE')
+        imagen.innerHTML = `
+        <source srcset="build/img/gallery/full/${i}.avif" type="image/avif">
+        <source srcset="build/img/gallery/full/${i}.webp" type="image/webp">
+        <img loading="lazy" width="200" height="300" src="build/img/gallery/thumb/${i}.jpg" alt="imagen galeria">
+`;
+        // imagen.src = `src/img/gallery/thumb/${i}.jpg`
+        imagen.alt = 'Imagen de galeria'
 
-        galeria.appendChild(imagen);
+        //EventHandler
+        imagen.onclick = function () {
+            modal(i);
+        }
+        galeria.appendChild(imagen)
     }
 
 }
+
+function unclick() {
+    const wrapper = document.querySelector('.btn-hero');
+    const question = document.querySelector('.question');
+    const yesBtn = document.querySelector('.yes-btn');
+    const noBtn = document.querySelector('.no-btn');
+    
+
+    yesBtn.addEventListener('click', () => {
+        question.innerHTML = "I want to kiss you too!";
+    });
+
+    noBtn.addEventListener('mouseover', () => {
+        const wrapperRect = wrapper.getBoundingClientRect();
+        const noRect = noBtn.getBoundingClientRect();
+    
+        // Ensure `i` and `j` are within the bounds of the wrapper
+        const i = Math.floor(Math.random() * (wrapperRect.width - noRect.width));
+        const j = Math.floor(Math.random() * (wrapperRect.height - noRect.height));
+    
+        noBtn.style.left = i + 'px';
+        noBtn.style.top = j + 'px';
+    });
+}
+
+function modal (i) {
+    const imagen = document.createElement('PICTURE');
+    imagen.innerHTML= `
+        <source srcset="build/img/gallery/full/${i}.avif" type="image/avif">
+        <source srcset="build/img/gallery/full/${i}.webp" type="image/webp">
+        <img loading="lazy" width="200" height="300" src="build/img/gallery/thumb/${i}.jpg" alt="imagen galeria">
+    `;
+
+    const modal = document.createElement('DIV')
+    modal.classList.add('modal');
+    modal.onclick = cerrarModal;
+
+    const body = document.querySelector('body')
+    body.classList.add('overflow')
+    body.appendChild(modal)
+
+    const botonCerrar = document.createElement('BUTTON')
+    botonCerrar.textContent = 'X'
+    botonCerrar.classList.add('cerrarModal')
+    botonCerrar.onclick = cerrarModal
+
+    modal.appendChild(imagen)
+    modal.appendChild(botonCerrar);
+}
+
+function cerrarModal (){
+    const modal = document.querySelector('.modal')
+    modal.classList.add('fade-out')
+    setTimeout(() => {
+        modal?.remove()
+
+        const body = document.querySelector('body')
+        body.classList.remove('ovewflow')
+    }, 500)
+}
+
